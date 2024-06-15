86773e05142b176d19636939ef5b0f6cc5ad07f8
RobertCrummett <rncrummett5@gmail.com>
2024-06-14 18:55:36 -0600

    Lua spaces background

--- spacing.lua
+++ spacing.lua
@@ -1,14 +1,36 @@
--- Compute column spacing
+lua_output_global = {}
 
+-- Compute column spacing
 local function tounits(val, units)
-  return "\\Convert[unit = " .. units .. "]{" .. val .. "}"
+        return "\\Convert[unit = " .. units .. "]{" .. val .. "}"
 end
+
+-- Compute Background Params
+local function backgroundanchor(margin)
+        local headery = 4
+        local anchorx = tostring(margin) .. "cm"
+        local anchory = tostring(margin + headery) .. "cm"
+        local bgwidth = "\\dimexpr \\paperwidth - " .. tostring(2 * margin) .. "cm \\relax"
+        lua_output_global["anchorx"] = anchorx
+        lua_output_global["anchory"] = anchory
+        lua_output_global["bgwidth"] = bgwidth
+end
+
+
+
 
 local function printer(val)
-  print("****************************")
-  print(val)
-  print("****************************")
-  tex.print(val)
+        print("\n*********************************")
+        print("Hello World")
+        print(val)
+        print("*********************************\n")
 end
 
-return {tounits = tounits, printer = printer}
+local function get_output(key)
+        return lua_output_global[key]
+end
+
+return {tounits = tounits,
+        printer = printer,
+        get = get_output,
+        bgxy = backgroundanchor}
